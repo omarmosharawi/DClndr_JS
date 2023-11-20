7df24b16bf2fd068b67f02afa58a83314ec36c9d
Omar Mohamed Sharawi <125395589+omarmosharawi@users.noreply.github.com>
2023-11-21 00:30:42 +0200

    DC

--- script.js
+++ script.js
@@ -68,3 +68,13 @@ daysTag.addEventListener('click', e => {
         renderCalendar();
         }
 })
+
+
+
+function setInterval(){
+    setTimeout(()=>{
+        date = new Date();
+        renderCalendar();
+        setInterval();
+    }, 500);
+}
